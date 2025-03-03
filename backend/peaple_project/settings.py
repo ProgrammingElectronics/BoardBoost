5195f1000d6a9f83046af9634d28bcdc030ad87b
programmingelectronics <michael@programmingelectronics.com>
2025-03-03 14:02:11 -0500

    chore: adjusted keys

--- backend/peaple_project/settings.py
+++ backend/peaple_project/settings.py
@@ -21,8 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-t-5q0isfrg-h-9zaus3&nys0)h8bn(ko2_k=jnk4#=)1tptavm"
 
 env = environ.Env(
     # Set default values
@@ -36,6 +34,7 @@ OPENAI_API_KEY = env("OPENAI_API_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG")
+SECRET_KEY = env("DJANGO_SECRET_KEY")
 
 ALLOWED_HOSTS = []
 
@@ -134,9 +133,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
 STATICFILES_DIRS = [
-    BASE_DIR / 'chat' / 'static',
+    BASE_DIR / "chat" / "static",
 ]
 
 # Default primary key field type
